e7e37f75be94ad6f435e82ac8886216635bd7f25
gust17 <gustavopantoja.ap@gmail.com>
2022-03-19 00:14:39 -0300

    Update geraratendimentofechado.blade.php

--- resources/views/recepcionista/geraratendimentofechado.blade.php
+++ resources/views/recepcionista/geraratendimentofechado.blade.php
@@ -8,73 +8,72 @@
             <div class="panel-heading">
                 <h3 class="panel-title">Dados para Agendamento</h3>
             </div>
-            <div class="panel-body">
-                <div class="col-md-12 continua">
-                    <div class="panel">
-                        <div class="panel-heading">Selecione o Dia/Horario para o seu Atendimento</div>
-                        <div id="cadastro" class="panel-body">
+
+            <div class="col-md-12 continua">
+                <div class="panel">
+                    <div class="panel-heading">Selecione o Dia/Horario para o seu Atendimento no {{ $setor->name }}</div>
+                    <div id="cadastro" class="panel-body">
 
 
 
-                            <form class="m-t" method="POST" action="{{ url('geraragendamento') }}">
-                                @csrf
+                        <form class="m-t" method="POST" action="{{ url('geraragendamento') }}">
+                            @csrf
 
 
-                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
 
-                                <!-- Email Address -->
+                            <!-- Email Address -->
 
+                            <br>
+
+                            <div class="form-group">
+                                <select class="form-control boleado" name="agenda" id="agenda">
+                                    <option value=""></option>
+                                    @forelse ($agendas as $agenda)
+                                        <option value="{{ $agenda->id }}">{{ $agenda->data_formated }} -
+                                            {{ $agenda->hora }}</option>
+
+                                    @empty
+                                    @endforelse
+                                </select>
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                <label for="">Descrição</label>
+                                <textarea class="form-control" name="descricao" id="" cols="30" rows="10"></textarea>
+                            </div>
+                            <!-- Password -->
+
+
+                            <br>
+                            <div class="checkbox">
+                                <label><input type="checkbox" id="checkbox1" name="novo" value="1">Atendimento a
+                                    Terceiro</label>
+
+
+                            </div>
+
+                            <div id="" class="form-group terceiro">
+                                <label for="">Outorgantes</label>
+                                <select class="form-control" name="terceiro_id" id="">
+                                    <option value=""></option>
+                                    @forelse (Auth::user()->terceiros as $terceiro)
+                                        <option value="{{ $terceiro->id }}">{{ $terceiro->name }}</option>
+                                    @empty
+                                    @endforelse
+                                </select>
+                            </div>
+
+
+                            <div class="d-grid gap-2">
+
+                                <button type="submit" class="btn btn-lg btn-success boleado btn-block">Agendar</button>
                                 <br>
+                            </div>
 
-                                <div class="form-group">
-                                    <select class="form-control boleado" name="agenda" id="agenda">
-                                        <option value=""></option>
-                                        @forelse ($agendas as $agenda)
-                                            <option value="{{ $agenda->id }}">{{ $agenda->data_formated }} -
-                                                {{ $agenda->hora }}</option>
-
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="">Descrição</label>
-                                    <textarea class="form-control" name="descricao" id="" cols="30" rows="10"></textarea>
-                                </div>
-                                <!-- Password -->
+                        </form>
 
 
-                                <br>
-                                <div class="checkbox">
-                                    <label><input type="checkbox" id="checkbox1" name="novo" value="1">Atendimento a
-                                        Terceiro</label>
-
-
-                                </div>
-
-                                <div id="" class="form-group terceiro">
-                                    <label for="">Outorgantes</label>
-                                    <select class="form-control" name="terceiro_id" id="">
-                                        <option value=""></option>
-                                        @forelse (Auth::user()->terceiros as $terceiro)
-                                            <option value="{{ $terceiro->id }}">{{ $terceiro->name }}</option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </div>
-
-
-                                <div class="d-grid gap-2">
-
-                                    <button type="submit" class="btn btn-lg btn-success boleado btn-block">Agendar</button>
-                                    <br>
-                                </div>
-
-                            </form>
-
-
-                        </div>
                     </div>
                 </div>
             </div>
@@ -85,4 +84,27 @@
         </div>
 
     </div>
+@endsection
+
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+
+
+            $(".terceiro").hide();
+        });
+    </script>
+    <script>
+        $('#checkbox1').change(function() {
+
+            if (this.checked) {
+
+                $(".terceiro").show();
+            } else {
+                $(".terceiro").hide();
+            }
+            $('#textbox1').val(this.checked);
+        });
+    </script>
 @endsection
